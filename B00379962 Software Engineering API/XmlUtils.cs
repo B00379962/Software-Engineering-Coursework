3ff544d11531bea09f00412d736edbc33433571e
B00379962 <56114858+B00379962@users.noreply.github.com>
2021-05-06 19:10:33 +0100

    Add files via upload

---
+++ B00379962 Software Engineering API/XmlUtils.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace DataAccessManager
+{
+    public static class XmlUtils
+    {
+        public static T ReadXML<T>(string filePath)
+        {
+            try
+            {
+                using (FileStream stream = new FileStream(filePath, FileMode.Open))
+                {
+                    var xsz = new XmlSerializer(typeof(T));
+                    // Return the deserialized the XML
+                    return (T)xsz.Deserialize(stream);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            return default;
+        }
+
+        public static bool WriteXML<T>(T classToSave, string filePath)
+        {
+            try
+            {
+                using (FileStream stream = new FileStream(filePath, FileMode.Create))
+                {
+                    var xsz = new XmlSerializer(typeof(T));
+                    // Serialized the XML
+                    xsz.Serialize(stream, classToSave);
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                return false;
+            }
+
+        }
+
+    }
+}
