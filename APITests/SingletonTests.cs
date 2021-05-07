6437876a159797e9b5e098486862c0261c1224fc
B00379962 <56114858+B00379962@users.noreply.github.com>
2021-05-07 10:04:24 +0100

    Add files via upload

---
+++ APITests/SingletonTests.cs
@@ -0,0 +1,43 @@
+﻿using AssetReservation;
+using DataAccessManager;
+using Xunit;
+
+namespace APITests
+{
+    public class SingletonTests
+    {
+        [Fact]
+        public void TestAssetReservationSingletonWorking()
+        {
+            // check asset reservation singleton is working correctly by comparing instances and ensuring there is only 1
+
+            SingletonAssetReservation testConnectionAssetReservation = SingletonAssetReservation.Instance;
+            SingletonAssetReservation testConnectionAssetReservation2 = SingletonAssetReservation.Instance;
+            SingletonAssetReservation testConnectionAssetReservation3 = SingletonAssetReservation.Instance;
+            SingletonAssetReservation testConnectionAssetReservation4 = SingletonAssetReservation.Instance;
+
+            Assert.True(testConnectionAssetReservation != null);
+            Assert.True(testConnectionAssetReservation2 == testConnectionAssetReservation);
+            Assert.True(testConnectionAssetReservation3 == testConnectionAssetReservation);
+            Assert.True(testConnectionAssetReservation4 == testConnectionAssetReservation);
+
+        }
+
+        [Fact]
+        public void TestDataAccessSingletonWorking()
+        {
+            // check data access singleton is working correctly by comparing instances and ensuring there is only 1
+
+            SingletonDataAccessManager testConnectionAssetReservation = SingletonDataAccessManager.Instance;
+            SingletonDataAccessManager testConnectionAssetReservation2 = SingletonDataAccessManager.Instance;
+            SingletonDataAccessManager testConnectionAssetReservation3 = SingletonDataAccessManager.Instance;
+            SingletonDataAccessManager testConnectionAssetReservation4 = SingletonDataAccessManager.Instance;
+
+            Assert.True(testConnectionAssetReservation != null);
+            Assert.True(testConnectionAssetReservation2 == testConnectionAssetReservation);
+            Assert.True(testConnectionAssetReservation3 == testConnectionAssetReservation);
+            Assert.True(testConnectionAssetReservation4 == testConnectionAssetReservation);
+
+        }
+    }
+}
